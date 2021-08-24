c8cbefe0707f02937371498ba553b8e7b9fea315
Mostafa Tawfiq <mostafa.mohammad.twfiq@gmail.com>
2021-08-25 01:55:04 +0200

    Second commit

---
+++ cmake-build-debug/CMakeFiles/MonstersHotel.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MonstersHotel.dir/main.cpp.obj"
+  "MonstersHotel.exe"
+  "MonstersHotel.exe.manifest"
+  "MonstersHotel.pdb"
+  "libMonstersHotel.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MonstersHotel.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
