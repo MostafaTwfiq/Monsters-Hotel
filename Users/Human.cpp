e6c84fbcce7994dfa2a915985839ef70b6eee504
Mostafa Tawfiq <mostafa.mohammad.twfiq@gmail.com>
2021-08-25 17:34:46 +0200

    Implemented Human class.

--- Users/Human.cpp
+++ Users/Human.cpp
@@ -1,5 +1,6 @@
-//
-// Created by mosta on 8/25/2021.
-//
-
 #include "Human.h"
+
+Human::Human(string name, int numOfNights, int numOfRooms, bool dryC, bool spaC)
+: Guest(move(name), HUMAN, 500, 70, 100, numOfNights, numOfRooms, dryC, spaC) {
+
+}
--- Users/Human.h
+++ Users/Human.h
@@ -1,8 +1,11 @@
 #ifndef MONSTERSHOTEL_HUMAN_H
 #define MONSTERSHOTEL_HUMAN_H
 
+#include "Guest.h"
 
-class Human {
+class Human : public Guest{
+
+    Human(string name, int numOfNights, int numOfRooms, bool dryC, bool spaC);
 
 };
 
